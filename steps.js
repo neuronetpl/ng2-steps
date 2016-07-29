882fe10ff744efe4f3b49f99e22d5b1812f4f35c
neuronet <neuronet@neuronet.pl>
2016-07-29 11:24:12 +0200

    clearing trash

--- steps.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var steps_service_1 = require('./steps.service');
-exports.StepsService = steps_service_1.StepsService;
-var step_directive_1 = require('./step.directive');
-exports.StepDirective = step_directive_1.StepDirective;
-var steps_component_1 = require('./steps.component');
-exports.StepsComponent = steps_component_1.StepsComponent;
-//export const STEPS:Array<any> = [StepsService,StepsComponent,StepDirective];
-//# sourceMappingURL=steps.js.map
