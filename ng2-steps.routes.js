9bfafe0c389682007b13cc4d327d03377587a46e
neuronet <neuronet@neuronet.pl>
2016-07-29 14:11:34 +0200

    cleanign up

--- ng2-steps.routes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var routes = [];
-exports.StepsRoutes = routes;
-//# sourceMappingURL=ng2-steps.routes.js.map
--- ng2-steps.routes.ts
+++
@@ -1,6 +0,0 @@
-import { StepsService } from './ng2-steps.service';
-
-
-let routes= [];
-
-export const StepsRoutes = routes;
